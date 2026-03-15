3cca0c17efc5cf85392590ad9e33fb844c2b0264
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-15 15:35:09 -0300

    9.9. Transformando aplicação em um Short-lived Microservice

--- src/main/java/com/algaworks/algashop/billingscheduler/infrastructure/CancelExpiredInvoiceRunner.java
+++ src/main/java/com/algaworks/algashop/billingscheduler/infrastructure/CancelExpiredInvoiceRunner.java
@@ -3,21 +3,21 @@ package com.algaworks.algashop.billingscheduler.infrastructure;
 import com.algaworks.algashop.billingscheduler.application.CancelExpiredInvoicesApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CancelExpiredInvoiceScheduler {
+public class CancelExpiredInvoiceRunner implements ApplicationRunner {
 
     private final CancelExpiredInvoicesApplicationService applicationService;
 
-    @Scheduled(fixedRate = 5000)
-    public void runTask() {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         log.info("Task started - Cancelling expired invoices.");
         applicationService.cancelExpiredInvoices();
         log.info("Task ended - Expired invoices.");
     }
-
 }
