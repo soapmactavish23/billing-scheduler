9a549bff414e6a906c9e4f649ec1edc78b405b9f
soapmactavish23 <ricknogueira1231@gmail.com>
2026-08-08 21:24:37 -0300

    1.4. Self-Registration Pattern em ação: Microsserviço como Client do Eureka

--- src/main/java/com/algaworks/algashop/billingscheduler/BillingSchedulerApplication.java
+++ src/main/java/com/algaworks/algashop/billingscheduler/BillingSchedulerApplication.java
@@ -2,7 +2,6 @@ package com.algaworks.algashop.billingscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class BillingSchedulerApplication {
