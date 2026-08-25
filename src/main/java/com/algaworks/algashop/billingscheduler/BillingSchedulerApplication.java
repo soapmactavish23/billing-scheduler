19f812110500993be49873b9f505ef9957fcb439
soapmactavish23 <ricknogueira1231@gmail.com>
2026-08-24 22:05:42 -0300

    4.6. Integração com Front-end do Admin

--- src/main/java/com/algaworks/algashop/billingscheduler/BillingSchedulerApplication.java
+++ src/main/java/com/algaworks/algashop/billingscheduler/BillingSchedulerApplication.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.billingscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class BillingSchedulerApplication {
